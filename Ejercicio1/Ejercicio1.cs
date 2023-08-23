473004c83103f4fd31f853e89a4f84c70e5801b9
FeQuiBa24 <feddeqb.fqb@gmail.com>
2023-08-23 01:33:51 -0300

    Ejercicio 1 de Practica 2

---
+++ Ejercicio1/Ejercicio1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio1.Ejercicio1
+{
+    public class Division
+    {
+        public static void Dividir(int i)
+        {
+            try
+            {
+                int cociente = i / 0;
+                Console.WriteLine("\nEl cociente es: " + cociente);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nTerminó de realizarse la operación.");
+            }
+        }
+    }
+}
---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ejercicio1.Ejercicio1;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número entero para el dividendo: ");
+            int dividendo = int.Parse(Console.ReadLine());
+            Division.Dividir(dividendo);
+            
+            Console.ReadKey();
+        }
+    }
+}
