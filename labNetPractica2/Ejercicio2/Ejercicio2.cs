78276b49d35969d2bfbc621080894e0d710f5c69
FeQuiBa24 <feddeqb.fqb@gmail.com>
2023-08-23 00:52:59 -0300

    Ejercicio 2 de Practica 2

---
+++ labNetPractica2/Ejercicio2/Ejercicio2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace labNetPractica2.Ejercicio2
+{
+    public class Division
+    {
+        public static void Dividir(Decimal i, Decimal j)
+        {
+            try
+            {
+                Decimal cociente = i / j;
+                Console.WriteLine("\nEl cociente es: " + cociente);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("\n¡Solo Chuck Norris divide por cero!");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nTerminó de realizarse la operación.");
+            }
+        }
+    }
+}
---
+++ labNetPractica2/Ejercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using labNetPractica2.Ejercicio2;
+
+namespace labNetPractica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            try{
+                Console.WriteLine("Ingrese un número entero para el dividendo: ");
+                Decimal dividendo = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("\nIngrese un número entero para el divisor: ");
+                Decimal divisor = Convert.ToDecimal(Console.ReadLine());
+                Division.Dividir(dividendo, divisor);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("\n¡Seguro ingresó una letra o no ingresó nada!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
